7b5fb832b97a8ae3a944c1e89d2657eb203998ed
Mayopi <cyade007@gmail.com>
2023-06-27 13:34:48 +0700

    console info

--- src/pages/api/surah/index.js
+++ src/pages/api/surah/index.js
@@ -13,10 +13,10 @@ export default async function handler(req, res) {
       delete updatedSurah.audio;
       updatedData.push(updatedSurah);
     });
-
     res.status(200).json({
       updatedData,
     });
+    console.info("Fetching All Data Complete");
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
